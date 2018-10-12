ae97fa369b80c33ce69566a1318e612b6cce6b16
Carlos Gonzalez <homearanya@gmail.com>
2018-10-12 10:21:36 +0200

    path to right markdown files folder

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,11 @@
 const path = require("path")
 
 exports.createPages = ({ actions, graphql }) => {
-    const { createPage } = actions
+  const { createPage } = actions
 
-    const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
+  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
 
-    return graphql(`
+  return graphql(`
     {
       allMarkdownRemark(
         sort: { order: DESC, fields: [frontmatter___date] }
@@ -21,16 +21,16 @@ exports.createPages = ({ actions, graphql }) => {
       }
     }
   `).then(result => {
-            if (result.errors) {
-                return Promise.reject(result.errors)
-            }
+      if (result.errors) {
+        return Promise.reject(result.errors)
+      }
 
-            result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-                createPage({
-                    path: node.frontmatter.path,
-                    component: blogPostTemplate,
-                    context: {}, // additional data can be passed via context
-                })
-            })
+      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.frontmatter.path,
+          component: blogPostTemplate,
+          context: {}, // additional data can be passed via context
         })
+      })
+    })
 }
